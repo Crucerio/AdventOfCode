002acae09a5866d8c3ba84c74f0f405e4b1bc3ce
Crucerio <cru@cruorus.mageha.de>
2020-12-01 22:02:22 +0100

    dayone, don't over engineer

---
+++ Day1/d1p1.ps1
@@ -0,0 +1,13 @@
+﻿#AOC Lesson 1
+$numbers=Get-Content ./Day1/puzzleinput1.txt|%{[System.int32]::Parse($_)}|Sort-Object
+$i=0
+$j=$numbers.Count-1
+while ($true){
+    if(($numbers[$i]+$numbers[$j]) -eq 2020){
+        return $($numbers[$i]*$numbers[$j])
+    }elseif ($numbers[$i]+$numbers[$j] -gt 2020) {
+        $j--
+    }elseif ($numbers[$i]+$numbers[$j] -lt 2020) {
+        $i++
+    }
+}
---
+++ Day1/d1p2.ps1
@@ -0,0 +1,11 @@
+﻿#AOC2020d1p2
+$numbers=Get-Content ./Day1/puzzleinput1.txt|%{[System.int32]::Parse($_)}|Sort-Object
+for ($i=0;$i -lt $numbers.Count;$i++){
+    for ($j=0;$j -lt $numbers.Count;$j++){
+        for ($k=0;$k -lt $numbers.Count;$k++){
+            if ($numbers[$i]+$numbers[$j]+$numbers[$k] -eq 2020){
+                return $numbers[$i]*$numbers[$j]*$numbers[$k]
+            }
+        }
+    }
+}
