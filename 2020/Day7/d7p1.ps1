dfb14c90a70314f81b6eed8dcb963cf046f14be3
Crucerio <cru@cruorus.mageha.de>
2020-12-09 01:44:42 +0100

    off by one fun ;)

--- 2020/Day7/d7p1.ps1
+++ 2020/Day7/d7p1.ps1
@@ -24,18 +24,28 @@ function Get-BagRule ($rule) {
 }
 $bagrules = New-Object 'system.collections.generic.dictionary[[string],[system.collections.generic.list[array]]]'
 $puzzleinput | ForEach-Object {
-    $key=Get-BagRule $_
-    $bagrules[$key.TopBag]=@($key.BagContent)
+    $key = Get-BagRule $_
+    $bagrules[$key.TopBag] = @($key.BagContent)
 }
 
-function Find-ValidTopBag ($bagrules,$contentbag) {
-    foreach ($topbag in $bagrules.Keys){
-        if($bagrules[$topbag].BagName -contains $contentbag){
+function Find-ValidTopBag ($bagrules, $contentbag) {
+    foreach ($topbag in $bagrules.Keys) {
+        if ($bagrules[$topbag].BagName -contains $contentbag) {
             $topbag
             Find-ValidTopBag -bagrules $bagrules -contentbag $topbag
         }
     }
 }
-$set=Find-ValidTopBag -bagrules $bagrules -contentbag "shiny gold bag"|
+$set = Find-ValidTopBag -bagrules $bagrules -contentbag "shiny gold bag" |
 Select-Object -Unique
 $set.count
+
+function Get-ContainedBags ($bagrules, $topbag, $multiply = 1) {
+    foreach ($contentbag in $bagrules[$topbag]) {
+        foreach ($subbag in $contentbag) {
+            $count=Get-ContainedBags -bagrules $bagrules -topbag $subbag.BagName -multiply $($multiply*$subbag.amount)
+        }
+    }
+    return $count+$multiply
+}
+(Get-ContainedBags -bagrules $bagrules -topbag "shiny gold bag") -1
