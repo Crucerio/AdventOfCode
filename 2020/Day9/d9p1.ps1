61695b651ee0b954a6041be0c17b2688693e1120
Crucerio <cru@cruorus.mageha.de>
2020-12-12 01:14:53 +0100

    d9_switch!

---
+++ 2020/Day9/d9p1.ps1
@@ -0,0 +1,38 @@
+$stopwatch =  [System.Diagnostics.Stopwatch]::StartNew()
+$stopwatch.Start()
+[array]$puzzleinput = Get-Content ./2020/Day9/puzzleinput9.txt|%{[Int64]::parse($_)}
+$preamble = 25
+$i = 0
+
+for ($i = $preamble; $i -lt $puzzleinput.Count; $i++) {
+    $att=$false
+    $pfui = $puzzleinput[($i - $preamble)..($i - 1)]
+    for ($j = 0; $j -lt $preamble; $j++) {
+        for ($k = 0; $k -lt $preamble; $k++) {
+            if (($pfui[$j] + $pfui[$k] -eq $puzzleinput[$i]) -and ($pfui[$j] -ne $pfui[$k])) {
+                $att = $true
+                continue
+            }
+        }
+    }
+    if (!$att){$answer1=$puzzleinput[$i] }
+}
+$answer1
+$l=0
+$h=1
+while($answer2sum -ne $answer1){
+    $sum=$puzzleinput[$l..$h]|
+    Measure-Object -Sum |
+    Select-Object -ExpandProperty sum
+    switch ($sum) {
+        {$_ -eq $answer1} { 
+            $return=$puzzleinput[$l..$h]|
+            Measure-Object -min -max|ForEach-Object{$_.minimum+$_.maximum}
+            return $return
+        }
+        {$_ -lt $answer1} { $h++ }
+        {$_ -gt $answer1} { $l++ }
+    }
+
+}
+$stopwatch.Elapsed| select *
