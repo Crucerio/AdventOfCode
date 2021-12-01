197a53a46c36abcbb3c99b16c7dfa7ad3d649f39
Crucerio <cru@cru>
2021-12-01 22:48:46 +0100

    2021 day 1

---
+++ 2021/2021_d1_script.ps1
@@ -0,0 +1,20 @@
+﻿
+$puzzle=Get-Content ./2021_d1_input1.txt|%{[System.Decimal]::Parse($_)}
+$j=0
+for ($i=1;$i -lt $puzzle.count;$i++ ){
+    if($puzzle[$i-1] -lt $puzzle[$i]){
+        $j++
+    }
+}
+"Part 1"
+$j
+$j=0
+for ($i=3;$i -lt $puzzle.count;$i++ ){
+    if(
+        $puzzle[$i-3] + $puzzle[$i-2] + $puzzle[$i-1] -lt
+        $puzzle[$i-2] + $puzzle[$i-1] + $puzzle[$i]
+        ){
+        $j++
+    }
+}
+$j
