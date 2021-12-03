4d1f105d5a4190d84317f7f6ed9c21c74b5e0528
Crucerio <cru@cru>
2021-12-03 21:44:54 +0100

    tidy

--- 2021/2021_d1_script.ps1
+++ 2021/2021_d1_script.ps1
@@ -1,5 +1,5 @@
 ﻿
-$puzzle=Get-Content ./2021_d1_input1.txt|%{[System.Decimal]::Parse($_)}
+$puzzle=Get-Content ./2021_d1_input1.txt|ForEach-Object{[System.Decimal]::Parse($_)}
 $j=0
 for ($i=1;$i -lt $puzzle.count;$i++ ){
     if($puzzle[$i-1] -lt $puzzle[$i]){
